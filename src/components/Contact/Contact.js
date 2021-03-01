70cd2501fe8c96f53a0007678b8009d24a893414
Foxdaddi <rensvdriel@hotmail.com>
2021-03-01 21:50:51 +0100

    V.1.0.2

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import '/Projects/portfolio/src/App.css';
+
+
+function Contact(){
+
+  // logica
+
+  return(
+    <div>
+    <div className = 'colorOne'></div>
+    
+  </div>
+  );
+}
+
+export default Contact;
